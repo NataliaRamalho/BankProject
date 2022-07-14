141e5883ec556912c4fd4d7eaeb2d3b1b414d6fb
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-07-14 20:17:52 -0300

    create method generateAccountNumber

--- backend/src/main/java/com/services/UserService.java
+++ backend/src/main/java/com/services/UserService.java
@@ -1,6 +1,7 @@
 package com.services;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,25 +9,35 @@ import org.springframework.stereotype.Service;
 import com.entities.User;
 import com.repositories.UserRepository;
 
-
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public User createUser(User u) {
+		u.balance = 0.0;
+		u.accountNumber = this.generateAccountNumber();
 		return repository.save(u);
 	}
-	
-	public List<User> findAll(){
+
+	private String generateAccountNumber() {
+		String accountNumber = "";
+		Random random = new Random();
+		for (int i = 0; i <= 6; i++) {
+			int newNumber = random.nextInt(9);
+			accountNumber = accountNumber + newNumber;
+		}
+		char letter = (char) (random.nextInt(26) + 'a');
+		return letter + accountNumber;
+	}
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	
+
 	public User seachUser(String email, String password) {
 		return repository.findByEmailAndPassword(email, password);
 	}
-	
 
 }
