cb878558e014a44192735eb9f8781db25c354e02
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-07-15 20:40:20 -0300

    Create method findByEmail

--- backend/src/main/java/com/repositories/UserRepository.java
+++ backend/src/main/java/com/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-	
+public interface UserRepository extends JpaRepository<User, Long> {
+
 	User findByEmailAndPassword(String email, String password);
+
+	User findByEmail(String email);
 }
