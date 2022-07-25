e5183ff45420bb115c7c0bce135aa63c3d984055
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-07-25 10:38:28 -0300

    Add exception handling

--- backend/src/main/java/com/resources/OperationResource.java
+++ backend/src/main/java/com/resources/OperationResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,31 +15,41 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.entities.Operation;
+import com.exceptions.CustomError;
 import com.services.OperationService;
 
 @RestController
 @RequestMapping(value = "/operations")
 public class OperationResource {
-	
+
 	@Autowired
 	private OperationService services;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Operation>> findAll(){
+	public ResponseEntity<List<Operation>> findAll() {
 		List<Operation> operations = services.findAll();
 		return ResponseEntity.ok().body(operations);
 	}
-	
-	@PostMapping(value="/{user_id}")
-	public ResponseEntity<Operation> registerUser(@PathVariable Long user_id, @RequestBody Operation op){ 
-	    op = services.createOperation(user_id, op);
-	    if(op == null) {
-	    	return ResponseEntity.notFound().build();
-	    }
-	    
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(op.getId()).toUri();
-		return ResponseEntity.created(uri).body(op);
+
+	@PostMapping(value = "/{user_id}")
+	public ResponseEntity<Object> registerUser(@PathVariable Long user_id, @RequestBody Operation op) {
+		try {
+			Operation[] ops = services.createOperation(user_id, op);
+			if (ops.length > 2) {
+				return ResponseEntity.notFound().build();
+			}
+
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(ops[0].getId())
+					.toUri();
+			return ResponseEntity.created(uri).body(op);
+		} catch (CustomError e) {
+			System.out.println(e);
+			return new ResponseEntity<>(e.getMessage(), e.getstatus());
+		} catch (Throwable e) {
+			System.out.println(e);
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+
 	}
-	
-	
+
 }
